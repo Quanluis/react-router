b61b18d6a38ea4d81c6bc8f4a3bb88e645a2be1d
Luis Garzon <luisgarzon90@yahoo.com>
2018-05-23 15:00:22 -0400

    working with react router on treehouse

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {
   BrowserRouter,
-  Route
+  Route, Switch
 } from 'react-router-dom';
 
 //App comonents 
@@ -10,15 +10,22 @@ import Home from './Home';
 import About from './About';
 import Teachers from './Teachers';
 import Courses from './Courses';
+import NotFound from './NotFound';
+import Featured from './Featured';
 
 const App = () => (
   <BrowserRouter>
   <div className="container">
   <Header />
-  <Route path exact = "/" component = {Home}/>
-  <Route path = "/about" render = {() => <About title = "About"/>} />
-  <Route path = "/teachers" component = {Teachers} />
-  <Route path = "/courses" component = {Courses} />
+
+  <Switch>
+    <Route path exact = "/" component = {Home}/>
+    <Route path = "/about" render = {() => <About title = "About"/>} />
+    <Route exact path = "/teachers" component = {Teachers} />
+    <Route path = '/teachers/:topic/:name' component = {Featured} />
+    <Route path = "/courses" component = {Courses} />
+    <Route component = {NotFound}/>
+  </Switch >
   </div>
   </BrowserRouter>
 );
